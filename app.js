d06e3035a6862cf34d353a88e64b950e59850497
bushrabhutto <106545621+bushrabhutto@users.noreply.github.com>
2023-05-03 11:21:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+//making a function that converts string into capital case
+function capitalCase(str) {
+    str = str.toUpperCase();
+    alert("string is in upper case" + str)
+
+}
+var str = prompt("enter a string");
+capitalCase(str);
